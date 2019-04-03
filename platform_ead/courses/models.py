c29e81cdf4a560fff1a5c5379ca8bf20312afe92
alessandroprudencio <alessandroconectado@gmail.com>
2019-04-02 20:48:02 -0400

    criando models e testando metodos

--- platform_ead/courses/models.py
+++ platform_ead/courses/models.py
@@ -6,6 +6,6 @@ class Course(models.Model):
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
     start_date = models.DateField('Data de Inicio', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/image', verbose_name="Image")
+    image = models.ImageField(upload_to='courses/image',null=True, blank=True, verbose_name="Image")
     created_at = models.DateTimeField('Criado em ', auto_now_add=True)
     update_at = models.DateTimeField('Atualizado em ', auto_now=True)
