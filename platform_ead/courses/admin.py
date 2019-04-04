b96f63505034fec6fed8eb948a30457ae8fa4eca
alessandroprudencio <alessandroconectado@gmail.com>
2019-04-03 20:32:58 -0400

    Editando painel adm do django

--- platform_ead/courses/admin.py
+++ platform_ead/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date' , 'created_at','image']
+    search_fields = ['name']
+    # prepopulated_fields = {"slug":("name",)}
+
+admin.site.register(Course, CourseAdmin)
--- platform_ead/courses/models.py
+++ platform_ead/courses/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(
+           models.Q(name__icontains=query) | \
+           models.Q(name__icontains=query))
+
 class Course(models.Model):
 
     name = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Atalho')
+    slug = models.SlugField('Categoria')
     description = models.TextField('Descrição', blank=True)
     start_date = models.DateField('Data de Inicio', null=True, blank=True)
     image = models.ImageField(upload_to='courses/image',null=True, blank=True, verbose_name="Image")
     created_at = models.DateTimeField('Criado em ', auto_now_add=True)
     update_at = models.DateTimeField('Atualizado em ', auto_now=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+        ordering = ['name']
